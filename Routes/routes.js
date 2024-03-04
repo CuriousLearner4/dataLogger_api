37c1e5a8b1fdc264af457db5d0ec8768e7017c53
Harsha Gopisetti <harshagopisetti@gmail.com>
2024-03-04 15:26:39 +0530

    update

--- Routes/routes.js
+++ Routes/routes.js
@@ -3,6 +3,7 @@ const Model = require('../Model/model');
 const router = express.Router();
 
 router.post('/post',async (req,res)=>{
+    console.log(req.body);
     const data = new Model({
         value: req.body.sensorValue
     });
@@ -11,7 +12,7 @@ router.post('/post',async (req,res)=>{
         res.status(200).json(dataToSave);
     }
     catch(error){
-        res.status(400).json({message:eroor.message});
+        res.status(400).json({message:error.message});
     }
 });
 
